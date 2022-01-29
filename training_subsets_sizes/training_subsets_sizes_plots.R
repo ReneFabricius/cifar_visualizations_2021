a561c02bd3a918ba0db8641085733aadf4cf3c37
ReneFabricius <fabricius@stud.uniza.sk>
2022-01-29 11:28:51 +0100

    Training subsets sizes individual slovak images.

--- training_subsets_sizes/training_subsets_sizes_plots.R
+++ training_subsets_sizes/training_subsets_sizes_plots.R
@@ -22,7 +22,8 @@ for (cp_m in unique(ens_metrics_c10$coupling_method))
         scale_x_log10(breaks=log_breaks(n=10)) +
         xlab("veľkosť trénovacej množiny") +
         ylab(metric_names[met_i]) +
-        ggtitle(paste0("Metrika ", metric_names[met_i], " pre rôzne veľkosti trénovacej množiny\npre pwcoup metódu ", toupper(cp_m)))
+        ggtitle(paste0("Metrika ", metric_names[met_i], " pre rôzne veľkosti trénovacej množiny\npre pwcoup metódu ", toupper(cp_m))) +
+        theme_bw()
     
     plot_name <- paste0("training_subsets_sizes/CIF10_", cp_m, "_", metric, ".pdf")
     ggsave(filename = plot_name, plot = box_plt, device = cairo_pdf)
@@ -34,7 +35,7 @@ ens_metrics_c100 <- read.csv("../data/data_tv_5000_c100/0/exp_subsets_sizes_trai
 nets_metrics_c100 <- read.csv("../data/data_tv_5000_c100/0/exp_subsets_sizes_train_outputs/net_metrics.csv", stringsAsFactors=TRUE)
 
 
-for (cp_m in unique(ens_metrics_c10$coupling_method))
+for (cp_m in unique(ens_metrics_c100$coupling_method))
 {
   for (met_i in seq_along(metrics))
   {
@@ -44,7 +45,8 @@ for (cp_m in unique(ens_metrics_c10$coupling_method))
         geom_boxplot(mapping=aes_string(x="train_size", y=metric, group="train_size")) +
         facet_grid(rows=vars(combining_method), scales="free") +
         scale_x_log10(breaks=log_breaks(n=10)) +
-        ggtitle(paste0("Metrika ", metric_names[met_i], " pre rôzne veľkosti trénovacej množiny\npre pwcoup metódu ", toupper(cp_m)))
+        ggtitle(paste0("Metrika ", metric_names[met_i], " pre rôzne veľkosti trénovacej množiny\npre pwcoup metódu ", toupper(cp_m))) +
+        theme_bw()
     
     plot_name <- paste0("training_subsets_sizes/CIF100_", cp_m, "_", metric, ".pdf")
     ggsave(filename = plot_name, plot = box_plt, device = cairo_pdf)
