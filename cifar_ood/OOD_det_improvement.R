f0836173b052026b675800a07f6fb265a9236be1
ReneFabricius <fabricius@stud.uniza.sk>
2022-10-03 18:39:38 +0200

    OOD detection improvements - added baseline.

--- cifar_ood/OOD_det_improvement.R
+++ cifar_ood/OOD_det_improvement.R
@@ -14,16 +14,24 @@ library(Rfast)
 plot_improvements <- function(dir, dts)
 {
     pwc_metrics <- read.csv(file.path(dir, "ens_pwc_metrics.csv"))
+    cal_metrics <- read.csv(file.path(dir, "ens_cal_metrics.csv"))
     net_metrics <- read.csv(file.path(dir, "net_metrics.csv"))
 
-    for (comb_id in unique(pwc_metrics[, "combination_id"]))
+    best_net_mets <- function(comb_id)
     {
+
         cur_net_mets <- net_metrics[as.logical(pwc_metrics[pwc_metrics$combination_id == comb_id, ][1, net_metrics$network]), ]
-        pwc_metrics[pwc_metrics$combination_id == comb_id, "best_net_MSP_AUROC"] <- max(cur_net_mets$MSP_AUROC)
-        pwc_metrics[pwc_metrics$combination_id == comb_id, "best_net_MLI_AUROC"] <- max(cur_net_mets$MLI_AUROC)
-        pwc_metrics[pwc_metrics$combination_id == comb_id, "best_net_MSP_AUPRC"] <- max(cur_net_mets$MSP_AUPRC)
-        pwc_metrics[pwc_metrics$combination_id == comb_id, "best_net_MLI_AUPRC"] <- max(cur_net_mets$MLI_AUPRC)
+        comb_mets <- data.frame(as.list(comb_id)) %>% mutate(
+            best_net_MSP_AUROC = max(cur_net_mets$MSP_AUROC),
+            best_net_MLI_AUROC = max(cur_net_mets$MLI_AUROC),
+            best_net_MSP_AUPRC = max(cur_net_mets$MSP_AUPRC),
+            best_net_MLI_AUPRC = max(cur_net_mets$MLI_AUPRC))
     }
+ 
+    best_net_aus <- dplyr::bind_rows(
+        apply(unique(pwc_metrics[c("combination_id")]), 1, best_net_mets))
+    pwc_metrics <- merge(pwc_metrics, best_net_aus)
+    cal_metrics <- merge(cal_metrics, best_net_aus)
 
     pwc_summary <- pwc_metrics %>% group_by(combining_method, coupling_method) %>% summarise(
         UNC_AUROC_o_max_MSP_AUROC = mean(UNC_AUROC - best_net_MSP_AUROC),
@@ -32,7 +40,7 @@ plot_improvements <- function(dir, dts)
         ENS_AUPRC_o_max_MSP_AUPRC = mean(MSP_AUPRC - best_net_MSP_AUPRC)
     )
 
-    au_imps <- pwc_metrics %>% 
+    au_imps_pwc <- pwc_metrics %>%
         mutate(
             UNC_AUROC_o_max_MSP_AUROC = UNC_AUROC - best_net_MSP_AUROC,
             UNC_AUPRC_o_max_MSP_AUPRC = UNC_AUPRC - best_net_MSP_AUPRC,
@@ -47,10 +55,28 @@ plot_improvements <- function(dir, dts)
             names_pattern="(.*?)_(.*?)_o_max_MSP_.*"
         )
 
-    plot <- ggplot(au_imps) +
-                geom_boxplot(mapping = aes(x = coupling_method, color = metric, y = value), position = "dodge") +
+    au_imps_cal <- cal_metrics %>%
+        mutate(
+            CAL_MSP_o_max_MSP_AUROC = MSP_AUROC - best_net_MSP_AUROC,
+            CAL_MSP_o_max_MSP_AUPRC = MSP_AUPRC - best_net_MSP_AUPRC) %>%
+        select(combination_id, calibrating_method, CAL_MSP_o_max_MSP_AUROC, CAL_MSP_o_max_MSP_AUPRC) %>%
+        pivot_longer(
+            cols = c(CAL_MSP_o_max_MSP_AUPRC, CAL_MSP_o_max_MSP_AUROC),
+            names_to = c("metric"),
+            names_pattern = "CAL_MSP_o_max_MSP_(.*?)$"
+        )
+
+    big_box_width <- length(unique(au_imps_pwc$coupling_method))
+    big_box_x <- (1 + big_box_width) / 2
+
+    plot <- ggplot() +
+                geom_boxplot(au_imps_pwc, mapping = aes(x = coupling_method, met_col = metric, y = value), position = "dodge") %>%
+                    relayer::rename_geom_aes(new_aes = c("colour" = "met_col")) +
+                geom_boxplot(au_imps_cal, mapping = aes(y = value, color = "TemperatureScaling", x = big_box_x), width = big_box_width) +
                 geom_hline(yintercept = 0, color = "green", linetype = "dashed") +
                 facet_grid(combining_method ~ ens_detection) +
+                scale_colour_brewer(aesthetics = "met_col", name = "metric", type = "qual") +
+                scale_color_manual(values = c("black"), name = "baseline") +
                 theme_classic()
     
     ggsave(
@@ -58,7 +84,7 @@ plot_improvements <- function(dir, dts)
         plot=plot)
     ggsave(
         filename = file.path("cifar_ood", paste0(dts, "_au_improvements_zoom.pdf")),
-        plot=plot + coord_cartesian(ylim = c(-0.1, 0.05)))
+        plot=plot + coord_cartesian(ylim = c(-0.05, 0.02)))
 
 }
 
